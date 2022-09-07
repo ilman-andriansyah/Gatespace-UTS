c00478cacbb0780900f19e4823c56e94ebcdf418
ilman-andriansyah <111233589+ilman-andriansyah@users.noreply.github.com>
2022-09-07 17:03:59 +0700

    Add files via upload

---
+++ database/deal_submit.php
@@ -0,0 +1,17 @@
+<html>
+<body bgcolor="black" style="font-family:helvetica;color:white;">
+<h3>DEAL SUBMIT
+<hr>
+<table>
+<?php
+echo "<tr><td>Nama Anda<td>".$_POST[ "nama"];
+echo "<tr><td>Username<td>".$_POST["username"];
+echo "<tr><td>Password<td>". $_POST["password"];
+echo "<tr><td>E-Mail<td>".$_POST["email"];
+echo "<tr><td>Kelamin<td>".$ POST ["kelamin"];
+echo "<tr><td>Hoby<td>".$_POST[ "musik"].$POST ["jalan"]. $_POST[ "baca"];
+echo "<tr><td>Jurusan<td>".$_POST[" jurusan"];
+echo "<tr><td>Keterangan<td>".$_POST["keterangan"];
+?>
+</body>
+</html>
